7804432c65e314e24c45a4971c7be2cd0bbabf2a
Babubhai <shubhsharma10@gmail.com>
2018-06-05 18:56:22 -0400

    Point to heroku java server

--- constants/index.js
+++ constants/index.js
@@ -1,35 +1,35 @@
-export const WIDGET_API_URL = 'http://10.0.0.185:8080/api/topic/TID/widget';
+export const WIDGET_API_URL = 'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/topic/TID/widget';
 export const LESSON_API_URL =
-    'http://10.0.0.185:8080/api/course/CID/module/MID/lesson';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/course/CID/module/MID/lesson';
 export const COURSE_API_URL =
-    'http://10.0.0.185:8080/api/course';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/course';
 export const MODULE_API_URL =
-    'http://10.0.0.185:8080/api/course/CID/module';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/course/CID/module';
 export const TOPIC_API_URL =
-    'http://10.0.0.185:8080/api/course/CID/module/MID/lesson/LID/topic';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/course/CID/module/MID/lesson/LID/topic';
 export const ASSIGNMENT_API_URL =
-    'http://10.0.0.185:8080/api/topic/TID/assignment';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/topic/TID/assignment';
 export const GEN_ASSIGNMENT_API_URL =
-    'http://10.0.0.185:8080/api/assignment/AID';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/assignment/AID';
 export const EXAM_API_URL =
-    'http://10.0.0.185:8080/api/topic/TID/exam';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/topic/TID/exam';
 export const GEN_EXAM_API_URL =
-    'http://10.0.0.185:8080/api/exam/EID';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/exam/EID';
 export const QUESTION_API_URL =
-    'http://10.0.0.185:8080/api/exam/EID/question';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/exam/EID/question';
 export const TRUEFALSE_API_URL =
-    'http://10.0.0.185:8080/api/exam/EID/truefalse';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/exam/EID/truefalse';
 export const GEN_TRUEFALSE_API_URL =
-    'http://10.0.0.185:8080/api/truefalse/QID';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/truefalse/QID';
 export const ESSAY_API_URL =
-    'http://10.0.0.185:8080/api/exam/EID/essay';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/exam/EID/essay';
 export const GEN_ESSAY_API_URL =
-    'http://10.0.0.185:8080/api/essay/QID';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/essay/QID';
 export const MULTIPLE_CHOICE_API_URL =
-    'http://10.0.0.185:8080/api/exam/EID/choice';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/exam/EID/choice';
 export const GEN_MULTIPLE_CHOICE_API_URL =
-    'http://10.0.0.185:8080/api/choice/QID';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/choice/QID';
 export const BLANKS_API_URL =
-    'http://10.0.0.185:8080/api/exam/EID/blanks';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/exam/EID/blanks';
 export const GEN_BLANKS_API_URL =
-    'http://10.0.0.185:8080/api/blanks/QID';
+    'https://cs5610-summer1-2018-ssharma.herokuapp.com/api/blanks/QID';
